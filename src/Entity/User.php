60f1afd59b5d06d10e756b2d878e6dd985d15d65
ZAYNA <zk10-91@hotmail.com>
2023-08-22 22:38:33 +0200

    add serializer groups

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,8 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+
+
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
@@ -11,19 +15,43 @@ class User
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['getUser'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['getUser'])]
+    #[Assert\NotBlank(message: "Veuillez renseigner un prénom", allowNull: false, normalizer: 'trim')]
+    #[Assert\Length(
+        min: 2,
+        max: 255,
+        minMessage: "Le prénom doit contenir {{ limit }} caractères minimum",
+        maxMessage: "Le prénom ne peut pas faire plus de  {{ limit }} caractères maximum"
+    )]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['getUser'])]
+    #[Assert\NotBlank(message: "Veuillez renseigner un nom", allowNull: false, normalizer: 'trim')]
+    #[Assert\Length(
+        min: 2,
+        max: 255,
+        minMessage: "Le nom doit contenir {{ limit }} caractères minimum",
+        maxMessage: "Le nom ne peut pas faire plus de  {{ limit }} caractères maximum"
+    )]
     private ?string $lastname = null;
 
     #[ORM\Column]
+    #[Groups(['getUser'])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'users')]
+    #[Groups(['getUser'])]
     private ?Client $client = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
